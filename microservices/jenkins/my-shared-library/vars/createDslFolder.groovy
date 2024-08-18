14c82306c698890f4f4c3d28849736b816266a8e
Выдрин Сергей <vydrin@vtb.ru>
2024-08-18 07:00:31 +0300

    autocommit

--- microservices/jenkins/my-shared-library/vars/createDslFolder.groovy
+++ microservices/jenkins/my-shared-library/vars/createDslFolder.groovy
@@ -1,8 +1,13 @@
 // vars/createDslFolder.groovy
 
 def call(String folderName) {
+    // Вывод текущего каталога
+    def currentDir = sh(script: 'pwd', returnStdout: true).trim()
+    echo "Current directory: ${currentDir}"
+
     // Загрузите скрипт из vars/dsl/createFolder.groovy
     def scriptText = libraryResource 'dsl/createFolder.groovy'
+    echo "Script content:\n${scriptText}"
 
     // Замените placeholder ${folderName} на фактическое значение
     scriptText = scriptText.replace('${folderName}', folderName)
