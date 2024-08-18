724c3de6ab8eb33025611b19273a05c374957b22
Выдрин Сергей <vydrin@vtb.ru>
2024-08-18 04:37:37 +0300

    autocommit

--- microservices/jenkins/my-shared-library/vars/createDslFolder.groovy
+++ microservices/jenkins/my-shared-library/vars/createDslFolder.groovy
@@ -1,7 +1,9 @@
 // vars/createDslFolder.groovy
 
 def call(String folderName) {
-    folder(folderName) {
-        description("This is the folder for ${folderName}")
-    }
+    jobDsl scriptText: """
+        folder('${folderName}') {
+            description("This is the folder for ${folderName}")
+        }
+    """
 }
