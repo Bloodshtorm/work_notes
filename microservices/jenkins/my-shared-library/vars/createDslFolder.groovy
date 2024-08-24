1c90aec7bc21e70bc3051bc4bf1be6dd4efc26bd
Выдрин Сергей <vydrin@vtb.ru>
2024-08-24 03:58:18 +0300

    autocommit

--- microservices/jenkins/my-shared-library/vars/createDslFolder.groovy
+++ microservices/jenkins/my-shared-library/vars/createDslFolder.groovy
@@ -1,17 +1,18 @@
-// vars/createDslFolder.groovy
+import dsl.*
 
 def call(String folderName) {
-    // Загрузите скрипт из vars/dsl/createFolder.groovy
-    def scriptText = libraryResource './dsl/createFolder.groovy'
-    echo "Script content:\n${scriptText}"
+    try {
+        // Получение скрипта из класса CreateFolder
+        def scriptText = CreateFolder.generateScript(folderName)
+        echo "Script content:\n${scriptText}"
 
-    // Замените placeholder ${folderName} на фактическое значение
-    scriptText = scriptText.replace('${folderName}', folderName)
-
-    // Запустите DSL скрипт с использованием jobDsl плагина
-    jobDsl scriptText: scriptText,
-           lookupStrategy: 'SEED_JOB',
-           ignoreExisting: false,
-           removedJobAction: "DISABLE",
-           sandbox: true
+        // Запуск DSL скрипта с использованием jobDsl плагина
+        jobDsl scriptText: scriptText,
+               lookupStrategy: 'SEED_JOB',
+               ignoreExisting: false,
+               removedJobAction: "DISABLE",
+               sandbox: true
+    } catch (Exception e) {
+        error "Ошибка при выполнении скрипта: ${e.message}"
+    }
 }
