cb01c85a2e8c6340794675561357d5d7a144a379
Выдрин Сергей <vydrin@vtb.ru>
2024-09-16 02:21:22 +0300

    autocommit

--- microservices/jenkins/my-shared-library/vars/createDslFolder.groovy
+++ microservices/jenkins/my-shared-library/vars/createDslFolder.groovy
@@ -1,6 +1,43 @@
-def call(String folderName) {
+import org.yaml.snakeyaml.Yaml
+import org.yaml.snakeyaml.constructor.Constructor
+
+def call() {
+    // Путь к файлу YAML
+    def yamlFilePath = "my-shared-library/vars/directory.yaml"
+
+    // Чтение YAML файла
+    def yamlFile = new File(yamlFilePath)
+    if (!yamlFile.exists()) {
+        error "YAML файл не найден: ${yamlFilePath}"
+    }
+
+    // Парсинг YAML файла
+    Yaml yaml = new Yaml(new Constructor(Map))
+    Map parsedYaml = yaml.load(yamlFile.text)
+
+    // Получаем список директорий
+    def directories = parsedYaml['directories']
+    generateFolders(directories)
+}
+
+def generateFolders(directories) {
+    directories.each { dir ->
+        if (dir instanceof Map) {
+            dir.each { parentDir, subDirs ->
+                createFolder(parentDir)
+                subDirs.each { subDir ->
+                    createFolder("${parentDir}/${subDir}")
+                }
+            }
+        } else {
+            createFolder(dir)
+        }
+    }
+}
+
+def createFolder(String folderName) {
     try {
-        // Прямо генерируем DSL-скрипт для создания папки
+        // Генерация и выполнение DSL скрипта для создания папок
         def scriptText = """
         folder('${folderName}') {
             description("This is the folder for ${folderName}")
@@ -8,13 +45,12 @@ def call(String folderName) {
         """
         echo "Скрипт сгенерирован:\n${scriptText}"
 
-        // Запуск DSL-скрипта с использованием плагина jobDsl
         jobDsl scriptText: scriptText,
-            lookupStrategy: 'SEED_JOB',
-            ignoreExisting: false,
-            removedJobAction: "DISABLE",
-            sandbox: false
+                lookupStrategy: 'SEED_JOB',
+                ignoreExisting: false,
+                removedJobAction: "DISABLE",
+                sandbox: false
     } catch (Exception e) {
-        error "Ошибка при выполнении скрипта: ${e.message}"
+        error "Ошибка при создании папки ${folderName}: ${e.message}"
     }
 }
