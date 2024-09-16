95edcaecf5db66bf3d58dd2acd369147a9c726b0
Выдрин Сергей <vydrin@vtb.ru>
2024-09-16 05:18:18 +0300

    autocommit

--- microservices/jenkins/my-shared-library/vars/createDslFolder.groovy
+++ microservices/jenkins/my-shared-library/vars/createDslFolder.groovy
@@ -1,20 +1,15 @@
+import org.yaml.snakeyaml.Yaml
+
 def call() {
-    // Загрузка YAML файла с использованием readYaml
+    // Загрузка YAML файла с использованием libraryResource
     def yamlContent = libraryResource 'vars/dirrectory.yaml'
-    def parsedYaml = readYaml text: yamlContent
-
-    if (!yamlContent) {
-        error "Не удалось загрузить YAML файл: ${yamlFilePath}"
-    }
-
     echo "YAML загружен: ${yamlContent}"
 
-    // Получаем список директорий
-    def directories = yamlContent['directories']
-    if (!directories) {
-        error "В YAML файле отсутствует ключ 'directories'"
-    }
+    // Парсинг YAML файла
+    def parsedYaml = readYaml text: yamlContent
 
+    // Получаем список директорий
+    def directories = parsedYaml['directories']
     echo "Директории: ${directories}"
     generateFolders(directories)
 }
